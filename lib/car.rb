d42197851865165e334b020c85b9ed23d4f5ec35
talryan <nataliamryan@gmail.com>
2020-11-04 18:50:52 -0800

    commit tree de7bf0e6dedc3c685dd61f56063fd1e03237ef83

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,13 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+    attr_accessor :size, :number 
+    def initialize(size, number)
+        @size = size 
+        @number = number
+    end
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+      end
+
+end
